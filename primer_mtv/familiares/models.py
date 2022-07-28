869814ea3ccee023dfd83e42f3adaeec9cdd61aa
aye sanchez <ayelen.sanchez@gmail.com>
2022-07-28 15:59:13 -0300

    'arreglo_models'

--- primer_mtv/familiares/models.py
+++ primer_mtv/familiares/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class familiares(models.Model):
+    date = models.DateField (auto_now_add = True, null = True, blank = True)
     name = models.CharField(max_length=20, default=True)
     last_name = models.CharField (max_length=20, default=True)
-    date_of_birth = models.DateField (auto_now_add = True, null = True, blank = True)
     age = models.IntegerField (default=True)
-    relationship = models.CharField (max_length=10)
     gender = models.CharField (max_length= 10, null=True, default=True)
+    relationship = models.CharField (max_length=10)
 
